3df15ebeecb307ac400288a2d9abcb08515cd56d
wrac4242 <42539664+wrac4242@users.noreply.github.com>
2021-03-20 12:48:42 +0000

    general plan added

---
+++ src/main.py
@@ -0,0 +1,5 @@
+#main python file to be run
+#uses processing.py file for modifying the array
+#main function is to deal with interfacing the array to the gui
+
+
---
+++ src/processing.py
@@ -0,0 +1,3 @@
+#main processing functions
+#deals with array modification
+#main focus for efficiency
